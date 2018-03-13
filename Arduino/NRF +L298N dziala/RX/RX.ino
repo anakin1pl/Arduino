9c3ba879227089a32056c831212c882d60fbdc52
Marcin Pawlukiewicz <35707757+anakin1pl@users.noreply.github.com>
2018-03-13 16:11:49 +0100

    Add files via upload

---
+++ Arduino/NRF +L298N dziala/RX/RX.ino
@@ -0,0 +1,87 @@
+#include <Servo.h> 
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+
+RF24 radio(9,10);
+
+const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };//2 pipes communication
+uint8_t received_data[3];
+uint8_t num_received_data =sizeof(received_data);
+
+const int servo1 = 8;//  servo
+
+Servo myservo;  // create servo object to control a servo
+int servoval;  // variable to read the value from the analog pin
+
+
+////
+const int IN1 = 5;
+const int IN2 = 4;
+const int IN3 = 6;
+const int IN4 = 7;
+
+const int ENA = 3;
+////
+
+void setup(void)
+{
+  Serial.begin(9600);
+  delay(300); //wait until the esc starts in case of Arduino got power first
+  myservo.attach(servo1);  // attaches the servo on pin 3 to the servo object 
+  myservo.write(90);
+
+  radio.begin();
+  radio.setPALevel(RF24_PA_LOW);
+  radio.setRetries(15,15);
+  radio.openReadingPipe(1,pipes[1]);
+  radio.startListening();
+  radio.printDetails();
+  radio.openWritingPipe(pipes[1]);
+  radio.openReadingPipe(1,pipes[0]);
+  radio.startListening();
+
+  pinMode (IN1, OUTPUT);
+  pinMode (IN2, OUTPUT);
+  pinMode (IN3, OUTPUT);
+  pinMode (IN4, OUTPUT);
+  pinMode (ENA, OUTPUT);
+
+}//void setup
+
+void loop(void)
+{
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  
+  ///////////////////////////////Radio reading///////////////////////////////////////////////////////     
+  if ( radio.available() )
+  {
+    bool done = false;
+    while (!done)
+    {
+    done = radio.read(&received_data, num_received_data  );
+    delay(30);
+    }// while (!done)
+   
+    Serial.print(received_data[0]);
+    Serial.print ("---"); 
+   // Serial.print(received_data[1]);
+   // Serial.print ("---"); 
+   // Serial.print(received_data[2]);
+
+    radio.stopListening();
+
+    radio.startListening();
+   
+ /****************************************steering servo*********************************************************************/
+    servoval = received_data[0];
+   // servoval = servoval*10;//scale back
+    servoval = map(servoval, 0, 102, 0, 200);//steering servo
+    Serial.println(servoval);
+    analogWrite(ENA, servoval);
+    delay(30);
+    
+
+  }// if ( radio.available() )
+}//void loop(void)
---
+++ Arduino/NRF +L298N dziala/TX/TX.ino
@@ -0,0 +1,65 @@
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+
+RF24 radio(9,10);
+
+uint8_t data[3] ; 
+const uint8_t buffer_size = sizeof(data);
+const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };//two pipes communication 
+
+const int analogInPinX = A0;// 
+const int analogInPinY= A1;
+
+
+int Str_value1 = 0;
+int Str_value2 = 0;  
+
+
+void setup(void)
+{
+  Serial.begin(9600);
+  pinMode(analogInPinX,INPUT );
+  pinMode(analogInPinY,INPUT );
+
+  radio.begin();
+  radio.setPALevel(RF24_PA_LOW);
+  radio.setRetries(15,15);
+  radio.openReadingPipe(1,pipes[1]);
+  radio.startListening();
+  radio.printDetails();
+  radio.openWritingPipe(pipes[0]);
+  radio.openReadingPipe(1,pipes[1]);
+  radio.stopListening();
+
+}
+
+void loop(void)
+{
+
+  Str_value1 = analogRead(analogInPinX);
+  Str_value2 = analogRead(analogInPinY); 
+
+  Str_value1 = Str_value1/10;//scaling Str_value1
+  data[0] = Str_value1;
+  Str_value2 = Str_value2/10;//scaling Str_value2
+  data[1] = Str_value2;
+  /////////////////////////////////////////////////////////////////////////
+  radio.stopListening();
+
+  bool ok = radio.write(  data ,sizeof(data) );
+  delay(30);
+  radio.startListening();
+  delay(20);
+  if (ok){
+    Serial.print("data[0]=");
+    Serial.print(data[0]);
+    Serial.print(" data[1]=");
+    Serial.print(data[1]);
+    Serial.print(" data[2]=");
+    Serial.println(data[2]);
+  }
+  else
+    Serial.println("Failed");
+
+}//void loop()
